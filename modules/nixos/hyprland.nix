e0959b21331be4685b1c0e72a6375a4468df940b
dr_Fade <eugenefade@gmail.com>
2026-07-18 09:43:03 +0300

    added display manager

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -5,6 +5,9 @@
     xwayland.enable = true;
   };
 
+  # Display manager: greetd with ReGreet (recommended DM for Hyprland per NixOS wiki)
+  programs.regreet.enable = true;
+
   # Essential runtime deps that need OS-level presence
   environment.systemPackages = with pkgs; [
     brightnessctl
